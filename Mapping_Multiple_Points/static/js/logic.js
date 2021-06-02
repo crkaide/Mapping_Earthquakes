275b0d78127d384a15f1a8d45b603f4beb3389cb
crkaide <crkaide@ilstu.edu>
2021-06-02 10:05:18 -0500

    finish skill drill

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -18,7 +18,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -82,13 +82,33 @@ streets.addTo(map);
 // }
 // ];
 
+// Get data from cities.js
+let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//   console.log(city)
+//   L.marker(city.location).addTo(map);
+// });
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.marker(city.location).addTo(map);
+// });
+
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
+cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location).addTo(map);
+  L.circleMarker(city.location, {
+    radius: city.population/100000,
+    fillColor: "orange",
+    color: "orange",
+    weight: 4
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
 });
-
-
 
 // // Add a circle to the map
 // L.circleMarker([34.0522, -118.2437], {
