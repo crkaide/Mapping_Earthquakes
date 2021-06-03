b9ba62075dd9a66f5078593f43264df2f4707656
crkaide <crkaide@ilstu.edu>
2021-06-03 14:24:25 -0500

    update Earthquakes_past7days/static/js/logicStep2.js

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -44,8 +44,9 @@ d3.json(earthquake_link).then(function(data) {
               console.log(data);
               return L.circleMarker(latlng);
           },
-          style: styleInfo
-    }).addTo(map);
+        // We set the style for each circleMarker using our styleInfo function.
+      style: styleInfo
+      }).addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
@@ -56,10 +57,19 @@ d3.json(earthquake_link).then(function(data) {
       fillOpacity: 1,
       fillColor: "#ffae42",
       color: "#000000",
-      radius: getRadius(),
+      radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
+  }
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
   }
 });
 
