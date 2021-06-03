f63b8d825d68a7499393b47fbff789e9076b40c9
crkaide <crkaide@ilstu.edu>
2021-06-03 14:16:15 -0500

    update Earthquakes_past7days/static/js/logicStep2.js

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -39,12 +39,28 @@ d3.json(earthquake_link).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
 
-  // We turn each feature into a circleMarker on the map.
+    // We turn each feature into a circleMarker on the map.
   pointToLayer: function(feature, latlng) {
               console.log(data);
               return L.circleMarker(latlng);
-          }
+          },
+          style: styleInfo
     }).addTo(map);
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
 });
 
 // Then we add our 'graymap' tile layer to the map.
