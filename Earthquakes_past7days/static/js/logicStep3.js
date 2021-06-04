44a17818f146296d6e3a4ef1492ae745c8eb2659
crkaide <crkaide@ilstu.edu>
2021-06-04 08:45:33 -0500

    Update Earthquakes_past7days/static/js/logicStep3.js

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -35,18 +35,20 @@ let earthquake_link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 
 // Grabbing our GeoJSON data.
 d3.json(earthquake_link).then(function(data) {
-  console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-
     // We turn each feature into a circleMarker on the map.
   pointToLayer: function(feature, latlng) {
-              console.log(data);
-              return L.circleMarker(latlng);
-          },
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
         // We set the style for each circleMarker using our styleInfo function.
-      style: styleInfo
-      }).addTo(map);
+    style: styleInfo,
+        // We create a popup for each circleMarker to display the magnitude and location of the earthquake after the marker has been created and styled.
+      onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
